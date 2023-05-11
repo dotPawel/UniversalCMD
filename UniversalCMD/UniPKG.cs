b1cda70d7df1739a9787e67b3bc5ab126270dbf0
Paweł <89011403+dotPawel@users.noreply.github.com>
2023-05-11 15:47:16 +0200

    Revert "UniPKG hotfix (5.2r)"
    This reverts commit d89f6efed2b2a0cd209efa06b234d42f4214c797.

--- UniversalCMD/UniPKG.cs
+++ UniversalCMD/UniPKG.cs
@@ -278,6 +278,7 @@ namespace UniCMD
                 OtherUtils.PrintException(ex);
                 Program.Prompt();
             }
+            Program.Prompt();
         }
         static void CheckPackageInfo(string PackageName)
         {
